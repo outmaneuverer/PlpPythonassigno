a2dd1b580b7d7bdb2fb5467dbcd778890979112e
outmaneuverer <mannaluoch@gmail.com>
2025-08-29 15:03:18 +0300

    week3

---
+++ Praco/Assignemnt2/Week3/Week3.py
@@ -0,0 +1,20 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = (discount_percent / 100) * price
+        final_price = price - discount_amount
+        return final_price
+    else:
+        return price
+
+# Prompting user for input
+original_price = float(input("Enter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+#The Call the function
+final_price = calculate_discount(original_price, discount_percent)
+
+# Printing
+if discount_percent >= 20:
+    print(f"Discount applied! The final price is: {final_price:.2f}")
+else:
+    print(f"No discount applied. The price remains: {final_price:.2f}")
